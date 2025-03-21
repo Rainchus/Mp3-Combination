f69bfa8d7c9e509e9824ca65a7044552865e249a
Rainchus <rainmachus@gmail.com>
2025-03-21 15:28:09 -0500

    bump version number

--- src/main.c
+++ src/main.c
@@ -768,7 +768,7 @@ void drawMessageOnBootLogos(void) {
     }
     if (mp3_osResetType == 0 && eepType == EEPROM_TYPE_16K) {
         mp3_debug_font_color = 4;
-        mp3_DrawDebugText(20, 212, "MOD BY: RAINCHUS VERSION 0.4.0B");
+        mp3_DrawDebugText(20, 212, "MOD BY: RAINCHUS V0.4.1B");
         mp3_DrawDebugText(20, 221, "IF YOU WOULD LIKE TO SUPPORT MY WORK:");
         mp3_DrawDebugText(20, 230, "HTTPS://KO-FI.COM/RAINCHUS");
     }
